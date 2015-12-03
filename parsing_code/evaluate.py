11a93cb251a3fe4b475b7a373d245e03c1c7f19b
Santina Lin <santina424@gmail.com>
2015-12-03 10:49:19 -0800

    Added comments
    - Comments for each function
    - TODOs for further improvement of the code

--- parsing_code/evaluate.py
+++ parsing_code/evaluate.py
@@ -4,6 +4,9 @@ from os.path import isfile, isdir, join
 import re 
 
 def buildAbstractExistDict(metadata):
+	''' Make and return a hash that records whether a paper has an abstract
+	using the line number of the abstracts in the metadata as the key 
+	'''
 	hasAbstract = {}
 	index = 0
 	with open(metadata) as f:
@@ -17,6 +20,10 @@ def buildAbstractExistDict(metadata):
 	return hasAbstract
 
 def buildRelevanceDict(metadata):
+	''' Make and return a hash that records the relevance score {'DR', 'PR', 'NR'}
+	(definitely relevance, probably relevance, not relevance) and the category 
+	for each paper, using the line number of the abstract in the metadata as the key 
+	'''
 	relevance = {}
 	index = 0
 	with open(metadata) as f:
@@ -27,6 +34,10 @@ def buildRelevanceDict(metadata):
 	return relevance
 
 def getPrecision(papers, hasAbstract, relevanceDict, category):
+	''' Get the precision of the predictions by checking whether 
+	the papers in the prediction are of the same category of the 
+	target paper and are also relevant 
+	'''
 	npapers = len(papers)
 	numCorrects = 0
 	for paperID in papers:
@@ -39,7 +50,11 @@ def getPrecision(papers, hasAbstract, relevanceDict, category):
 			npapers -= 1
 	return float(numCorrects)/npapers
 
+# TODO: implement different metrics of measuring a score (eg recall, MAP)
 def getScore(result, hasAbstractDict, relevanceDict, npapers):
+	''' Get a score for the prediction by calling getPrecision() 
+	return None if the target paper has no abstract
+	'''
 	
 	IDs = [int(i) for i in result.split()]
 	paper = IDs[0]
@@ -50,12 +65,16 @@ def getScore(result, hasAbstractDict, relevanceDict, npapers):
 		return getPrecision(IDs[1:npapers+1], hasAbstractDict, relevanceDict, category)
 
 def recordScore(out, npapers, totalScore, matrixFolder, distanceFolder, closestPapersFile):
+	''' Write the average score for a particular combination of a type matrix, a distance function, 
+	and number of singular values used to an outfile file `out` 
+	'''
 	avgScore = str(float(totalScore)/npapers)
 	nsv = re.findall(r'\d+', closestPapersFile)[0]
 
 	out.write('\t'.join([matrixFolder, distanceFolder, closestPapersFile, nsv, avgScore]) + '\n')
 
 def main():
+	# TODO : use argparse to read in input files and other parameters 
 
 	metadata_wAbstracts = "../info_data/abstracts.txt"
 	resultFolder = "/projects/slin_prj/slin_prj_results/closest_papers/TREC2005_train"
@@ -69,6 +88,8 @@ def main():
 	hasAbstractDict = buildAbstractExistDict(metadata_wAbstracts)
 	relevanceDict = buildRelevanceDict(metadata_wAbstracts)
 	nPapers = 20
+
+	# TODO : wrap this chunk in a separate function 
 
 	# traversing the file 
 	for matrixFolder in [f for f in listdir(resultFolder) if isdir(join(resultFolder, f))]:
